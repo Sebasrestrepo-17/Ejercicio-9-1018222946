48e7a944e1a6ad880e397e5f6d1bf5df65c6c709
Sebas <sebas4831441@gmail.com>
2024-10-27 16:36:41 -0500

    Se inicia nuevo repositorio sobre el ejercicio#9

---
+++ script.js
@@ -0,0 +1,5 @@
+//Planteamiento del Problema:
+//Un concesionario necesita un sistema que permita calcular las cuotas mensuales de
+//los clientes que adquieren un vehículo financiado. Las tasas de interés varían según
+//el plazo del préstamo.
+
